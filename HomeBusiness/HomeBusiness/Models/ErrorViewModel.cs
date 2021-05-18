96325b4a89808efb0140c077a0eee6bcafd4c843
AshaSaha-27 <57867179+AshaSaha-27@users.noreply.github.com>
2021-05-18 01:13:49 -0400

    first commit

---
+++ HomeBusiness/HomeBusiness/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeBusiness.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
