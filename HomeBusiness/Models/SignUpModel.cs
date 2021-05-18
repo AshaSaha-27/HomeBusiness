4bfaaa6e36e52db7024db904a49cd409a0d45d8e
AshaSaha-27 <57867179+AshaSaha-27@users.noreply.github.com>
2021-05-18 01:18:14 -0400

    second

---
+++ HomeBusiness/Models/SignUpModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeBusiness.Models
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = "Please Enter your email")]
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter your Password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please Enter your ConfirmPassword")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
